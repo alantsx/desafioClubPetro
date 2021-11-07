ec207119ec788e7025b25986e225846e18c58c7e
Alan <alanpfabricio@gmail.com>
2021-11-06 21:48:56 -0300

    call user using axios

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const fs = require('fs');
 const axios = require('axios');
 
-router.get('/', (req, res) => {
-  axios.get('https://randomuser.me/api').then((response) => {
-    console.log(response);
-  })
+router.get('/users', async (req, res) => {
+  const { data } = await axios.get('https://randomuser.me/api');
+  res.json(data);
+});
+
+router.get('/users/teste', async (req, res) => {
+  const { data } = await axios.get('https://randomuser.me/api/?uuid=c5c0aa82-3d64-4d67-808d-04d1dc0309e1');
+  res.json(data);
 });
 
 module.exports = router;
